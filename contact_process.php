4ba7d6e0ebe851ad0983427ef842ca66a9ea2631
AbdelrahmanElalfee <abdelrahmanelalfee@gmail.com>
2022-12-17 09:29:09 +0200

    banners

--- contact_process.php
+++ contact_process.php
@@ -1,46 +1,41 @@
 <?php
-include 'PHPMailer/src/Exception.php';
-include 'PHPMailer/src/PHPMailer.php';
-include 'PHPMailer/src/SMTP.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-if(isset($_POST['send'])){
+if (isset($_POST['send'])) {
 
-$to = 'CS@elnouroptics.net';
-$myMail = 'abdelrahmanelalfee@gmail.com';
-$name = $_POST['name'];    
-$msg = $_POST['message'];    
-$subject = $_POST['subject'];    
-$email = $_POST['email'];    
-$mail = new PHPMailer;
+    $to = 'CS@elnouroptics.net';
+    $myMail = 'abdelrahmanelalfee@gmail.com';
+    $smtpPass = 'koxqeobcyoxdnxqo';
+    $name = $_POST['name'];
+    $msg = $_POST['message'];
+    $subject = $_POST['subject'];
+    $email = $_POST['email'];
+    $mail = new PHPMailer;
 
-// $mail->SMTPDebug = 3;                  // Enable verbose debug output
+    // $mail->SMTPDebug = 3;                  // Enable verbose debug output
 
-$mail->isSMTP();                          // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';           // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                   // Enable SMTP authentication
-$mail->Username = $myMail;                // SMTP username
-$mail->Password = 'koxqeobcyoxdnxqo';     // SMTP password
-$mail->Port = 587;                        // TCP port to connect to
+    $mail->isSMTP();                          // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';           // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                   // Enable SMTP authentication
+    $mail->Username = $myMail;                // SMTP username
+    $mail->Password = $smtpPass;              // SMTP password
+    $mail->Port = 587;                        // TCP port to connect to
 
-$mail->setFrom($myMail, 'Website', 1);
-$mail->addAddress($to);                   // Add a recipient
-$mail->Subject= $subject;
-$mail->Body= $msg;
-$mail->Body.= " My Name: ";
-$mail->Body.= $name;
-$mail->Body.= " My email address: ";
-$mail->Body.= $email;
+    $mail->setFrom($myMail, 'Website', 1);
+    $mail->addAddress($to);                   // Add a recipient
+    $mail->Subject = $subject;
+    $mail->Body = $msg;
+    $mail->Body .= " My Name: ";
+    $mail->Body .= $name;
+    $mail->Body .= " My email address: ";
+    $mail->Body .= $email;
 
-if(!$mail->send()) {
-    echo "<script> alert('Message could not be sent.'); </script>";
-    header("refresh:1;url=index.html");
-} else {
-    echo "<script> alert('Mail sent successfuly.'); </script>";
-    header("Refresh: 1; URL=index.html");
+    if (!$mail->send()) {
+        echo "<script> alert('Message could not be sent.'); </script>";
+        header("refresh:1;url=index.html");
+    } else {
+        echo "<script> alert('Mail sent successfuly.'); </script>";
+        header("Refresh: 1; URL=index.html");
+    }
 }
-}
-?>
